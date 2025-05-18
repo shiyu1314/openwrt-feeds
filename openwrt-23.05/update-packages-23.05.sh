d3aadb89001e3afaa5ccca6027eb3037a8fb447c
shiyu1314 <shiyu1052@gmail.com>
2025-05-19 06:24:49 +0800

    更新 update-packages-23.05.sh

--- openwrt-23.05/update-packages-23.05.sh
+++ openwrt-23.05/update-packages-23.05.sh
@@ -229,7 +229,7 @@ sed -i 's/"order": 1050/"order": 80/g' luci-app-zerotier/root/usr/share/luci/men
 rm -rf luci-theme-material3/{.git,Readme.md}
 sed -i 's|../../luci.mk|$(TOPDIR)/feeds/luci/luci.mk|' luci-theme-material3/Makefile
 sed -i '/uci -q delete luci.themes.Material3Red/a \	uci set luci.main.mediaurlbase=\x27/luci-static/bootstrap\x27' luci-theme-material3/Makefile
-sed -i "s/changed=0/changed=1/" luci-theme-material3/root/etc/uci-defaults/30_luci-theme-material3
+chmod +x luci-theme-material3/root/etc/uci-defaults/30_luci-theme-material3
 
 # ddns-scripts
 mv immortalwrt/packages/net/ddns-scripts ./
