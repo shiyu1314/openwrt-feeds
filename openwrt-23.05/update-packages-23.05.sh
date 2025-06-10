23291489b5479badf4ae2996238c6a8586cfffde
shiyu1314 <shiyu1052@gmail.com>
2025-06-10 13:14:12 +0800

    更新 update-packages-23.05.sh

--- openwrt-23.05/update-packages-23.05.sh
+++ openwrt-23.05/update-packages-23.05.sh
@@ -53,7 +53,7 @@ mv lucky/*/ ./
 
 git clone https://github.com/happyzhang1995/openwrt-adguardhome adguardhome --depth 1
 rm -rf adguardhome/{.git,LICENSE,README.md}
-sed -i "s/PKG_ARCH_ADGUARDHOME:=arm/PKG_ARCH_ADGUARDHOME:=armv7" adguardhome/Makefile
+sed -i "s/PKG_ARCH_ADGUARDHOME:=arm/PKG_ARCH_ADGUARDHOME:=armv7/" adguardhome/Makefile
 
 sed -i "s/Arch="armv7"/Arch="armv7"/" luci-app-adguardhome/root/usr/share/AdGuardHome/update_core.sh
 
