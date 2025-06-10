9ed1d681097022d93d60b9053d8c80d090e4cae8
shiyu1314 <shiyu1052@gmail.com>
2025-06-10 12:52:38 +0800

    更新 update-packages-23.05.sh

--- openwrt-23.05/update-packages-23.05.sh
+++ openwrt-23.05/update-packages-23.05.sh
@@ -51,6 +51,9 @@ git clone https://github.com/sirpdboy/luci-app-lucky lucky -b main --depth 1
 rm -rf lucky/{.git,doc,README.md}
 mv lucky/*/ ./
 
+git clone https://github.com/happyzhang1995/openwrt-adguardhome adguardhome --depth 1
+rm -rf adguardhome/{LICENSE,README.md}
+
 # pkgs
 mv openwrt_pkgs/*/ ./
 mv openwrt-packages/*/ ./
