82ad3bd20389f30ae147165ff9d858614c199c82
shiyu1314 <shiyu1052@gmail.com>
2025-09-26 15:29:35 +0800

    Update update-packages-23.05.sh

--- openwrt-23.05/update-packages-23.05.sh
+++ openwrt-23.05/update-packages-23.05.sh
@@ -324,6 +324,9 @@ sed -i 's/nas/services/' luci-app-syncthing/luasrc/controller/syncthing.lua
 mv immortalwrt/packages/utils/syncthing ./
 sed -i 's|../../lang|$(TOPDIR)/feeds/packages/lang|' syncthing/Makefile
 
+git clone https://github.com/theosoft-git/luci-app-easymesh luci-app-easymesh --depth 1
+rm -rf luci-app-easymesh/{.git,README.md}
+
 
 rm -rf openwrt immortalwrt openwrt-alist ariang-nginx openwrt-alist doc
 ls -d */ | xargs -n 1 basename | paste -sd ' ' - > packages.txt
